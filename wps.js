816e5bef74761f547024a8e25ce1b3f0387d97ee
tomas <tomas@logand.com>
2010-01-23 14:26:10 +0100

    Changes from 2009-06-24

--- wps.js
+++ wps.js
@@ -1,10 +1,21 @@
-function PdfT(V) {
-  this.V = V;
+function Symbol(N) {
+  if("/" == N[0]) {
+    this.nm = N.substring(1);
+    this.q = true;
+  } else this.nm = N;
   return this;
 }
 
-function isPdfT(V) {
-  return V && V.constructor == PdfT; // TODO better test
+function isSymbol(V) {
+  return V &&  V.constructor == Symbol;
+}
+
+function isQuoted(V) {
+  return V.q;
+}
+
+function symbolName(V) {
+  return V.nm;
 }
 
 function ps0(L, F, S) {
@@ -54,7 +65,7 @@ function ps0(L, F, S) {
       }
       if(C !== false) L.push(C);
     }
-    return new PdfT(L.join(""));
+    return L.join("");
   }
 
   function symbol() {
@@ -72,7 +83,7 @@ function ps0(L, F, S) {
     }
     L = L.join("");
     if(1 == L.length && member(L, "+-.")) N = false;
-    return N ? (F ? parseFloat(L) : parseInt(L, 10)) : L;
+    return N ? (F ? parseFloat(L) : parseInt(L, 10)) : new Symbol(L);
   }
 
   function proc() {
@@ -100,22 +111,17 @@ function ps0(L, F, S) {
     }
   }
 
-//   function quoted(T) {
-//      return typeof T == "string" && "/" == T.charAt(0);
-//   }
-
   function parse(E) {
     var G = true;
     while(G && peek()) {
       var T = token();
       if(T || T == 0) {
-        if(typeof T == "number" || typeof T == "object" || quoted(T))
-          S.push(T);
-        else {
-          if(F[T]) F[T]();
-          else throw "Unknown operator '" + T + "' " + typeof T;
-          if(E == T) G = false;
-        }
+        if(isSymbol(T) && !isQuoted(T)) {
+          var X = symbolName(T);
+          if(F[X]) F[X]();
+          else throw "Unknown operator '" + X + "'";
+          if(E == X) G = false;
+        } else S.push(T);
       }
     }
     return S;
@@ -124,60 +130,35 @@ function ps0(L, F, S) {
   return parse();
 }
 
-function quoted(T) {
-  return typeof T == "string" && "/" == T.charAt(0);
-}
-
 function wps(E, T) {
   var S = [];
   var F = {};
   var C = E.getContext("2d");
 
-  F["get"] = function() { // dict key -- any
-    var K = S.pop();
-    var D = S.pop();
-    // TODO other datatypes http://www.capcode.de/help/get
-    S.push(D[K.substring(1)]);
-  };
-  F["put"] = function() { // dict key any --
-    var V = S.pop();
-    var K = S.pop();
-    var D = S.pop();
-    // TODO other datatypes http://www.capcode.de/help/put
-    D[K.substring(1)] = V;
-  };
-
+  // trivial
   F["true"] = function() {S.push(true);};
   F["false"] = function() {S.push(false);};
   F["null"] = function() {S.push(null);};
 
-  F["neg"] = function() {S.push(- S.pop());};
+  // math
+  F["neg"] = function() {S.push(-1 * S.pop());};
   F["add"] = function() {S.push(S.pop() + S.pop());};
   F["mul"] = function() {S.push(S.pop() * S.pop());};
-  F["div"] = function() {
-    var X = S.pop();
-    S.push(S.pop() / X);
-  };
-  F["idiv"] = function() {
-    var X = S.pop();
-    S.push(Math.floor(S.pop() / X));
-  };
-  F["mod"] = function() {
-    var X = S.pop();
-    S.push(S.pop() % X);
-  };
-  // TODO sqrt, exp, ceiling, sin
+  F["div"] = function() {var X = S.pop();S.push(S.pop() / X);};
+  F["mod"] = function() {var X = S.pop();S.push(S.pop() % X);};
+
+  // stack
   F["exch"] = function() {
     var Y = S.pop();
     var X = S.pop();
     S.push(Y);
     S.push(X);
   };
-
   F["dup"] = function() {var X = S.pop(); S.push(X); S.push(X);};
   F["clear"] = function() {S.length = 0;};
   F["pop"] = function() {S.pop();};
-  F["roll"] = function() { // n j --
+  F["index"] = function() {S.push(S[S.pop()]);}; // TODO from other end!!!
+  F["roll"] = function() {
     var J = S.pop();
     var N = S.pop();
     var X = [];
@@ -188,43 +169,18 @@ function wps(E, T) {
     for(I = 0; I < J; I++) S.push(X.shift());
     for(I = 0; I < N - J; I++) S.push(Y.shift());
   };
-
-  F["eq"] = function() {
-    var Y = S.pop();
-    var X = S.pop();
-    S.push(X == Y);
-  };
-  F["ne"] = function() {
-    var Y = S.pop();
-    var X = S.pop();
-    S.push(X != Y);
-  };
-  F["gt"] = function() {
-    var Y = S.pop();
-    var X = S.pop();
-    S.push(X > Y);
-  };
-  F["lt"] = function() {
-    var Y = S.pop();
-    var X = S.pop();
-    S.push(X < Y);
-  };
-  F["ge"] = function() {
-    var Y = S.pop();
-    var X = S.pop();
-    S.push(X >= Y);
-  };
-  F["le"] = function() {
-    var Y = S.pop();
-    var X = S.pop();
-    S.push(X <= Y);
+  F["copy"] = function() {
+    var N = S.pop();
+    for(var I = 0; I < N; I++)
+      S.push(S[N - 1]); // TODO from other end!!!
   };
 
-  F["if"] = function() {
-    var B = S.pop();
-    var C = S.pop();
-    if(C == true) run(B);
-  };
+  F["eq"] = function() {var Y = S.pop(); var X = S.pop(); S.push(X == Y);};
+  F["lt"] = function() {var Y = S.pop(); var X = S.pop(); S.push(X < Y);};
+
+  //F["not"] = function() {var X = S.pop(); S.push(X == undefined || X == false);};
+  //F[".nand"] = function() {S.push(F["not"]() || F["not"]());};
+
   F["ifelse"] = function() {
     var N = S.pop();
     var P = S.pop();
@@ -256,7 +212,6 @@ function wps(E, T) {
   };
 
   F["."] = function() {alert(S.pop());};
-  F["=="] = function() {alert(S[0]);};
   F["pstack"] = function() {alert(S);};
 
   function run(C) {
@@ -265,61 +220,91 @@ function wps(E, T) {
       var M = C.length;
       for(var I = 0; I < M; I++) {
         var T = C[I];
-        if(typeof T == "number" || typeof T == "object" || quoted(T))
-          S.push(T);
-        else {
-          if(F[T]) F[T]();
-          else throw "Unknown operator '" + T + "' " + typeof T;
-        }
+        if(isSymbol(T) && !isQuoted(T)) {
+          var X = symbolName(T);
+          if(F[X]) F[X]();
+          else throw "Unknown operator '" + X + "'";
+        } else S.push(T);
       }
     }
   }
 
+  F["get"] = function() { // dict key -- any
+    var K = S.pop();
+    var D = S.pop();
+    // TODO other datatypes http://www.capcode.de/help/get
+    S.push(D[K]);
+  };
+  F["put"] = function() { // dict key any --
+    var V = S.pop();
+    var K = S.pop();
+    var D = S.pop();
+    // TODO other datatypes http://www.capcode.de/help/put
+    D[K] = V;
+  };
+
   F["def"] = function() {
     var C = S.pop();
     var N = S.pop();
-    if(quoted(N)) F[N.substring(1)] = function() {run(C);};
-    else throw "Wrong operator name " + N + " for " + C;
+    if(isSymbol(N) && isQuoted(N)) F[symbolName(N)] = function() {run(C);};
+    else throw "Wrong operator name '" + N + "' as '" + typeof N
+      + "' for '" + C + "'";
   };
 
   // js ffi operators
 
-  F["call"] = function() { // dict key nargs -- ...
+  F[".call"] = function() { // dict key nargs -- result
     var N = S.pop();
     var K = S.pop();
     var D = S.pop();
     var X = [];
-    for(var I = 0; I < N; I++) {
-      var V = S.pop();
-      X.unshift(isPdfT(V) ? V.V : (quoted(V) ? V.substring(1) : V));
-    }
-    D[K.substring(1)].apply(D, X);
+    for(var I = 0; I < N; I++) X.unshift(S.pop());
+    S.push(D[K].apply(D, X));
   };
 
-  F["gc"] = function() { // -- gc
+  F[".gc"] = function() { // -- gc
     S.push(C);
   };
-
-  F["nativeString"] = function() { // any -- jsString
-    var V = S.pop();
-    S.push(isPdfT(V) ? V.V : (quoted(V) ? V.substring(1) : "" + V));
+  F[".math"] = function() { // -- Math
+    S.push(Math);
   };
 
   // html5 utility operators
 
-  F["rgb"] = function() {
+  // TODO js ffi to manipulate strings so the following can be in ps
+  F[".rgb"] = function() {
     var B = S.pop();
     var G = S.pop();
     var R = S.pop();
-    S.push(new PdfT("rgb(" + R + "," + G + "," + B + ")"));
+    S.push("rgb(" + R + "," + G + "," + B + ")");
   };
-  F["rgba"] = function() {
+  F[".rgba"] = function() {
     var A = S.pop();
     var B = S.pop();
     var G = S.pop();
     var R = S.pop();
-    S.push(new PdfT("rgba(" + R + "," + G + "," + B + "," + A + ")"));
+    S.push("rgba(" + R + "," + G + "," + B + "," + A + ")");
   };
+
+  // "junk" for tiger.eps
+
+  F["save"] = function() {S.push(true);};
+  F["restore"] = function() {S.push(true);};
+  F["bind"] = function() {};
+  F["dict"] = function() {};
+  F["load"] = function() {};
+  F["begin"] = function() {};
+  F["end"] = function() {};
+
+  F["where"] = function() {};
+  //F["currentflat"] = function() {S.push(0);};
+  //F["setflat"] = function() {};
+  //F["clippath"] = function() {};
+  F["transform"] = function() {};
+  //F["itransform"] = function() {};
+  //F["currentpoint"] = function() {S.push(0); S.push(0);};
+  F["*"] = function() {};
+  //F["showpage"] = function() {};
 
   if(T.length)
     for(var I = 0; I < T.length; I++)
